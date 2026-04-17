d1b745a52a84d9d3d19e423d81f4a11ef75f5439
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-04-17 22:18:34 +0100

    feat(api): construct profile model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Gender(models.TextChoices):
+        MALE = "male", "Male"
+        FEMALE = "female", "Female"
+        OTHERS = "others", "Others"
+    
+    class AgeGroup(models.TextChoices):
+        CHILD = "child", "Child"
+        TEEN = "teenager", "Teenager"
+        ADULT = "adult", "Adult"
+        SENIOR = "senior", "Senior"
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=Gender.choices)
+    gender_probability = models.DecimalField(max_digits=3, decimal_places=2)
+    sample_size = models.PositiveIntegerField()
+    age = models.PositiveSmallIntegerField()
+    age_group = models.CharField(max_length=10, choices=AgeGroup.choices)
+    country_id = models.CharField(max_length=5)
+    country_probability = models.DecimalField(max_digits=3, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
