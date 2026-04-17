97b1417f3a9208e8769302053c4c183ef872f8ed
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-04-17 22:19:46 +0100

    feat(api): implement services for external apis

---
+++ api/services.py
@@ -0,0 +1,50 @@
+import requests
+
+BASE_TIMEOUT = 5
+
+
+class ExternalAPIError(Exception):
+    def __init__(self, api_name):
+        self.api_name = api_name
+        super().__init__(api_name)
+
+
+def genderize(name):
+    res = requests.get("https://api.genderize.io", params={"name": name}, timeout=BASE_TIMEOUT)
+    data = res.json()
+
+    if res.status_code >= 500 or data.get("gender") is None or data.get("count") in (None, 0):
+        raise ExternalAPIError("Genderize")
+
+    return {
+        "gender": data["gender"],
+        "probability": data["probability"],
+        "count": data["count"]
+    }
+
+
+def agify(name):
+    res = requests.get("https://api.agify.io", params={"name": name}, timeout=BASE_TIMEOUT)
+    data = res.json()
+
+    if res.status_code >= 500 or data.get("age") is None:
+        raise ExternalAPIError("Agify")
+
+    return {"age": data["age"]}
+
+
+def nationalize(name):
+    res = requests.get("https://api.nationalize.io", params={"name": name}, timeout=BASE_TIMEOUT)
+    data = res.json()
+
+    countries = data.get("country")
+
+    if res.status_code >= 500 or not countries:
+        raise ExternalAPIError("Nationalize")
+
+    best = max(countries, key=lambda x: x["probability"])
+
+    return {
+        "country_id": best["country_id"],
+        "probability": best["probability"]
+    }
